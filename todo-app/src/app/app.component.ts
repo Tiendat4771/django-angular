35c3e75afd40cedeeb5a428d243aac1c087a6d8e
DatNT76 <DatNT76@fsoft.com.vn>
2019-03-01 16:11:10 +0700

    Init angular

---
+++ todo-app/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  providers: [TodoDataService]
+})
+export class AppComponent {
+
+  newTodo: Todo = new Todo();
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token `TodoDataService`
+  // and assign it to a property called `todoDataService`
+  constructor(private todoDataService: TodoDataService) {
+    this.todoDataService = todoDataService;
+  }
+
+  // Service is now available as this.todoDataService
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+}
---
+++ todo-app/src/app/todo-data.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import {Todo} from './todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoDataService {
+
+  // automatic inscrementing of ids
+  lastId: number = 0;
+
+  // Placehoder for todos
+  todos: Todo[] = [];
+  constructor() { }
+
+
+  // Add Todo
+  addTodo(todo: Todo): TodoDataService {
+    if (!todo.id) {
+      todo.id = ++this.lastId;
+    }
+    this.todos.push(todo);
+    return this;
+  }
+
+  // Simulate DELETE /todos/:id
+  deleteTodoById(id: number): TodoDataService {
+    this.todos = this.todos
+      .filter(todo => todo.id !== id);
+    return this;
+  }
+
+  // Simulate PUT /todos/:id
+  updateTodoById(id: number, values: object = {}): Todo {
+    const todo = this.getTodoById(id);
+    if (!todo) {
+      return null;
+    }
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  // Simulate GET /todos
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  // Simulate GET /todos/:id
+  getTodoById(id: number): Todo {
+    return this.todos
+      .filter(todo => todo.id === id)
+      .pop();
+  }
+
+  // Toggle todo complete
+  toggleTodoComplete(todo: Todo) {
+    const updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
+  }
+}
